d3c93488e24d52a90432fbc46f6bb2c621b8cd4f
mkalansari7 <mk.alansari7@gmail.com>
2022-01-19 18:10:55 +0300

    done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-
-import './App.css';
-import Header from './Components/Header';
-import IslandForm from './Components/IslandForm';
-import IslandList from './Components/IslandList';
-
+import "./App.css";
+import Header from "./Components/Header";
+import IslandForm from "./Components/IslandForm";
+import IslandList from "./Components/IslandList";
+import islands from "./data/islands";
+import React, { useState } from "react";
 
 function App() {
-
-  let island = {
+  const [island, setIsland] = useState({
     name: "Boubyan",
-    img: "http://photos.wikimapia.org/p/00/02/20/48/92_big.jpg"
-  }
+    img: "http://photos.wikimapia.org/p/00/02/20/48/92_big.jpg",
+  });
+
+  const p = (islandId) => {
+    setIsland(islandId);
+  };
+  // let island = {
+  //   name: "Boubyan",
+  //   img: "http://photos.wikimapia.org/p/00/02/20/48/92_big.jpg",
+  // };
 
   return (
-
     <div>
       <Header />
-
       <div className="homePage">
-        <IslandList />
+        <IslandList p={p} />
         <IslandForm island={island} />
       </div>
     </div>
--- src/Components/Island.js
+++ src/Components/Island.js
@@ -1,8 +1,11 @@
 import React from "react";
 
-export default function Island({ island }) {
+export default function Island({ island, p }) {
+  const settingIsland = () => {
+    p(island);
+  };
   return (
-    <div className="Island">
+    <div onClick={settingIsland} className="Island">
       <h3>{island.name}</h3>
       {/* YOU HAVE TO CHANGE THE VISITORS COUNT */}
       <p>Visitors 0</p>
